ad56a4f0463b8d837080f6459d57e7da41b5a45e
arbazz-siddique <arbazzsiddiquecse@gmail.com>
2025-09-02 13:26:14 +0530

    Implement dashboard and connect with the backend

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,54 +1,82 @@
-import React, { useEffect, useState } from 'react'
-import { dummyCreationData } from '../assets/assets';
-import { Gem, Sparkle } from 'lucide-react';
-import { Protect } from '@clerk/clerk-react';
-import CreationItems from '../components/CreationItems';
+import React, { useEffect, useState } from "react";
+import { dummyCreationData } from "../assets/assets";
+import { Gem, Sparkle } from "lucide-react";
+import { Protect } from "@clerk/clerk-react";
+import CreationItems from "../components/CreationItems";
+import axios from "axios";
+import { useAuth } from "@clerk/clerk-react";
+import toast from "react-hot-toast";
+
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
 
 const Dashboard = () => {
   const [creations, setCreations] = useState([]);
-  const getDashboardData = async ()=>{
-    setCreations(dummyCreationData)
-  }
-  useEffect(()=>{
-    getDashboardData()
-  },[])
+  const [loading, setLoading] = useState(true);
+  const { getToken } = useAuth();
+
+  const getDashboardData = async () => {
+    try {
+      const { data } = await axios.get("/api/user/get-user-creaations", {
+        headers: { Authorization: `Bearer ${await getToken()}` },
+      });
+      if (data.success) {
+        setCreations(data.creations);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getDashboardData();
+  }, []);
+
   return (
-    <div className='h-full overflow-y-scroll p-6'>
-      <div className='flex justify-start gap-4 flex-wrap'>
-        <div className='flex justify-between items-center w-72 p-4 px-6 bg-white rounded-xl border border-gray-200'>
-          <div className='text-slate-600'>
-            <p className='text-sm'>Total Creations</p>
-            <h2 className='text-xl font-semibold'>{creations.length}</h2>
+    <div className="h-full overflow-y-scroll p-6">
+      <div className="flex justify-start gap-4 flex-wrap">
+        <div className="flex justify-between items-center w-72 p-4 px-6 bg-white rounded-xl border border-gray-200">
+          <div className="text-slate-600">
+            <p className="text-sm">Total Creations</p>
+            <h2 className="text-xl font-semibold">{creations.length}</h2>
           </div>
 
-          <div className='w-10 h-10 rounded-lg bg-gradient-to-br from-[#3588F2] to-[#0BB0D7] text-white flex justify-center items-center'>
-            <Sparkle className='w-5 text-white'/>
+          <div className="w-10 h-10 rounded-lg bg-gradient-to-br from-[#3588F2] to-[#0BB0D7] text-white flex justify-center items-center">
+            <Sparkle className="w-5 text-white" />
           </div>
         </div>
 
-        <div className='flex justify-between items-center w-72 p-4 px-6 bg-white rounded-xl border border-gray-200'>
-          <div className='text-slate-600'>
-            <p className='text-sm'>Active Plan</p>
-            <h2 className='text-xl font-semibold'>
-              <Protect plan="premium" fallback="free">Premium</Protect>
+        <div className="flex justify-between items-center w-72 p-4 px-6 bg-white rounded-xl border border-gray-200">
+          <div className="text-slate-600">
+            <p className="text-sm">Active Plan</p>
+            <h2 className="text-xl font-semibold">
+              <Protect plan="premium" fallback="free">
+                Premium
+              </Protect>
             </h2>
           </div>
 
-          <div className='w-10 h-10 rounded-lg bg-gradient-to-br from-[#FF61C5] to-[#9E53EE] text-white flex justify-center items-center'>
-            <Gem className='w-5 text-white'/>
+          <div className="w-10 h-10 rounded-lg bg-gradient-to-br from-[#FF61C5] to-[#9E53EE] text-white flex justify-center items-center">
+            <Gem className="w-5 text-white" />
           </div>
         </div>
       </div>
-
-      <div className='space-y-3'>
-      <p className='mt-6 mb-4'>Recent Creationns</p>
-      {
-        creations.map((item)=> <CreationItems key={item.id} item={item}/>)
-      }
-      </div>
-
+      {loading ? (
+        <div className="flex justify-center items-center h-full">
+          <div className="w-12 h-12 rounded-full bg-gradient-to-r from-blue-500 to-purple-500 animate-ping"></div>
+        </div>
+      ) : (
+        <div className="space-y-3">
+          <p className="mt-6 mb-4">Recent Creationns</p>
+          {creations.map((item) => (
+            <CreationItems key={item.id} item={item} />
+          ))}
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
